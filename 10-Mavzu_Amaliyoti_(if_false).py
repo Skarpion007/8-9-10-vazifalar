affdaf7801d2ac4a449c76185b6c6ff4d7babb85
Skarpion007 <skarpion022@gmail.com>
2025-04-30 16:07:18 +0500

    Add files via upload

---
+++ 10-Mavzu_Amaliyoti_(if_false).py
@@ -0,0 +1,47 @@
+# 1-amaliyot
+
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+    
+#     if car == "gm":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+    #2-amaliyot    
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+    
+#     if car != "gm":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+#     #3-amaliyot
+# login = input("loginni kirit:")
+# if login == "admin":
+#     print(f"Xush kelibsiz", login)
+# else:
+#     print(f"login xato terildi qaytadan urinib koring", login)
+   
+#     #4-amaliyot
+# a = int(input("1-sonni kirit:")) 
+# b = int(input("2-sonni kirit:")) 
+# if a == b:
+#     print("ikkala son teng")
+# else:
+#     print("ten emas")
+
+#     #5-amaliyot
+# a = int(input("istalgan sonni kirit:")) 
+# if a >= 0:
+#     print("Musbat son")
+# else:
+#     print("Manfiy son")
+
+  #6-amaliyot
+import math
+a = float(input("istalgan sonni kirit:")) 
+if a >= 0:
+    print(f"sonning ildizi", a ** (1/2))
+else:
+    print("Musbat son kirit")
---
+++ 9-Mavzu_(For takrorlash)_amaliyoti.py
@@ -0,0 +1,29 @@
+#1-2-amaliyot
+
+# ismalr = ["Ali", "Vali", "Gani", "Sardor", "Bobur"]
+# for n in ismalr:
+#     print(f"Salom",n)
+# print(f"Kod {len(ismalr)} marta takrorlandi")
+
+
+# 3-amaliyot
+
+# toq_sonlar = list(range(11, 100, 2))
+# for n in toq_sonlar:
+   
+#     print(f" toq sonlarning kubi ", n ** 3)
+
+# #4-amaliyot
+# Kinolar = []
+# for kino in range(5):
+#     Kinolar.append(input(f"Sevimli kinoingizni kiriting:"))
+# print(f"Sizning eng sevimli Kinolaringiz shular:\n",Kinolar)
+
+# 5-amaliyot
+
+a = int(input("bugun necha kishi bilan uchrashdingiz?"))
+uchrashilganlar = []
+for n in range(a):
+    uchrashilganlar.append(input(f" {n+1}-uchrashgan insoningiz ismini kiriting:"))
+print(f"siz bugun uchrashgan barcha insonlar royxati:\n", uchrashilganlar)
+
